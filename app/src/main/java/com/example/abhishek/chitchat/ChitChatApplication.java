fb180c93c59e045d3a2f82b12d9edf17ed98410f
Abhishek Desai <abhishek.desai70@gmail.com>
2016-10-25 22:27:58 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/abhishek/chitchat/ChitChatApplication.java
@@ -0,0 +1,27 @@
+package com.example.abhishek.chitchat;
+
+import android.app.Application;
+
+import com.example.abhishek.chitchat.utils.Config;
+import com.parse.Parse;
+import com.parse.interceptors.ParseLogInterceptor;
+
+/**
+ * Created by abhishek on 10/25/16.
+ */
+
+public class ChitChatApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // set applicationId and server based on the values in the Heroku settings.
+        // any network interceptors must be added with the Configuration Builder given this syntax
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(Config.APPLICATION_ID) // should correspond to APP_ID env variable
+                .addNetworkInterceptor(new ParseLogInterceptor())
+                .server(Config.APPLICATION_URL).build());
+
+    }
+}
---
+++ app/src/main/java/com/example/abhishek/chitchat/utils/ConfigExample.java
@@ -0,0 +1,12 @@
+package com.example.abhishek.chitchat.utils;
+
+/**
+ * Created by abhishek on 10/25/16.
+ */
+
+public class ConfigExample {
+
+    public static final String APPLICATION_URL = "https://example.herokuapp.com/parse/";
+    public static final String APPLICATION_ID  = "YOUR_APPLICATION_ID";
+
+}
